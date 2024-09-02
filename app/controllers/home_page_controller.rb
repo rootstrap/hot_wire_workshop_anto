54ecc03b606ffe85e558a4eeea4aedc32194b62f
antoalconti <eizamusica6@gmail.com>
2024-09-02 14:41:14 +0200

    Add turbo

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class HomePageController < ApplicationController
   before_action :skip_authorization, :skip_policy_scope
 
-  def index
-  end
+  def index; end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,9 @@
 class ProductsController < ApplicationController
   before_action :skip_authorization, :skip_policy_scope, only: :index
 
-
   def index
     if params[:q].present?
-      @pagy, @products = pagy(Product.where("name LIKE ?", "%#{params[:q]}%"))
+      @pagy, @products = pagy(Product.where('name LIKE ?', "%#{params[:q]}%"))
     else
       query = Product.order(id: :desc)
       @pagy, @products = pagy(query, items: 3)
